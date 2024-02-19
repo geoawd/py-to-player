581f98778810015c6db5def86d29d309c20c8bb8
Alex <119129964+geoawd@users.noreply.github.com>
2024-02-19 21:16:29 +0000

    Update py-to-player.py

--- py-to-player.py
+++ py-to-player.py
@@ -5,6 +5,14 @@ from mfrc522 import SimpleMFRC522
 import RPi.GPIO as GPIO
 import pygame
 
+def connect():
+     try:
+        os.system('echo "disconnect 44:3D:54:B9:30:55" | bluetoothctl')
+        os.system('echo "connect 44:3D:54:B9:30:55" | bluetoothctl')
+        os.system('pacmd set-default-sink bluez_sink.44_3D_54_B9_30_55.a2dp_sink')
+     except Exception as e:
+        print(e)
+
 def playaudio(id):
     try:
         pygame.mixer.music.load('/home/alexdonald/Music/' +str(id) + '.mp3')
@@ -15,9 +23,6 @@ def playaudio(id):
 
 def shuffleaudio(id):
     try:
-       print("play each file in the directory in a random order")
-       # use random to play each file in the directory
-
        audio_files = [f for f in os.listdir('/home/alexdonald/Music/' +str(id)) if f.endswith(('.mp3'))]
        random.shuffle(audio_files)
 
@@ -25,29 +30,25 @@ def shuffleaudio(id):
            file_path = '/home/alexdonald/Music/' +str(id) + '/' + audio_file
            pygame.mixer.music.load(file_path)
            pygame.mixer.music.play()
-           while pygame.mixer.music.get_busy():
-               continue
              
     except Exception as e:
         print(e)
 
-def stopaudio():
-    try:
-        if pygame.mixer.music.get_busy() == True:
-            pygame.mixer.music.stop()
-            print("Pausing: " + str(id))
-    except Exception as e:
-        print(e)
+class Pause(object):
 
-def connect():
-     try:
-        os.system('echo "disconnect 44:3D:54:B9:30:55" | bluetoothctl')
-        os.system('echo "connect 44:3D:54:B9:30:55" | bluetoothctl')
-        os.system('pacmd set-default-sink bluez_sink.44_3D_54_B9_30_55.a2dp_sink')
-     except Exception as e:
-        print(e)
+    def __init__(self):
+        self.paused = pygame.mixer.music.get_busy()
+
+    def toggle(self):
+        if self.paused:
+            pygame.mixer.music.unpause()
+        if not self.paused:
+            pygame.mixer.music.pause()
+        self.paused = not self.paused
 
 pygame.init()
+
+pause = Pause()
 
 while True:
     try:
@@ -61,9 +62,8 @@ while True:
             if id == 584616865049: #helper card to connect to bluetooth
                 connect()
                 print("Connecting")
-            elif id == 428997447724: #helper card to stop any audio playing
-                stopaudio()
-                print("Stopping audio")
+            elif id == 428997447724: #helper card to pause/resume music
+                pause.toggle()
             elif os.path.isfile('/home/alexdonald/Music/' +str(id) + '.mp3'):
                 playaudio(id)
             elif os.path.isdir('/home/alexdonald/Music/' +str(id)):
@@ -77,4 +77,3 @@ while True:
     finally:
         print("Cleaning  up...")
         GPIO.cleanup()
-
